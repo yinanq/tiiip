611b17387c1579f54e931fa60fe3462d219a9865
Yinan <nickycarving@gmail.com>
2015-12-31 15:20:44 -0800

    Initial Commit

---
+++ tiiip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiiip
+//
+//  Created by Yinan iMac on 12/31/15.
+//  Copyright © 2015 YinanLearningSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
